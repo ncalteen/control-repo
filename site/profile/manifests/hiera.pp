31be6f2152d66c701e05e955182f5a57757fb276
ncalteen <ncalteen@gmail.com>
2018-01-16 11:16:01 -0800

    Create hiera.pp

---
+++ site/profile/manifests/hiera.pp
@@ -0,0 +1,17 @@
+class profile::hiera {
+  class { 'hiera':
+    hiera_version        =>  '5',
+    hiera5_defaults      =>  {"datadir" => "/etc/puppetlabs/code/environments/%{::environment}/hieradata", "data_hash" => "yaml_data"},
+    hierarchy            =>  [
+      {"name" => "Virtual yaml", "path"  =>  "virtual/%{::virtual}.yaml"},
+      {"name" => "Nodes yaml", "paths" =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
+      {"name" => "Default yaml file", "path" =>  "common.yaml"},
+    ],
+    eyaml                =>  true,
+    eyaml_gpg            =>  true,
+    eyaml_gpg_recipients =>  'mark@example.com,chris@example.com',
+    create_keys          =>  false,
+    keysdir              =>  '/etc/puppetlabs/code-staging/keys',
+    provider             =>  puppetserver_gem,
+  }
+}
